aa810424357edf7ad747cd5354ae6c203589dbcf
aditiokamal <aditiokamal@gmail.com>
2022-05-11 15:34:48 +0700

    update file variiable.dart

--- bin/variable.dart
+++ bin/variable.dart
@@ -1,4 +1,18 @@
-void main(){
+//tanpa menggunakan variable
+/*void main(){
+
+  print('Muhammad Aditio Kamal Ibrahim');
+
+  print('Muhammad Aditio Kamal Ibrahim');
+
+  print('Muhammad Aditio Kamal Ibrahim');
+
+  print('Muhammad Aditio Kamal Ibrahim');
+
+}*/
+
+//menggunakan variable
+/*void main(){
 
   String name;
 
@@ -11,5 +25,14 @@ void main(){
   print(name);
 
   print(name);
-}
 
+}*/
+
+//menggunakan variable deklarasi langsung
+void main(){
+  String name='Muhammad Aditio Kamal Ibrahim';
+  print(name);
+  print(name);
+  print(name);
+  print(name);
+}
