c10ff2d459141988348095b5b7a4d1d3dc5f2334
aditiokamal <aditiokamal@gmail.com>
2022-05-11 15:22:08 +0700

    belajar dart dasar

---
+++ bin/variable.dart
@@ -0,0 +1,11 @@
+void main(){
+
+  print("Muhammad Aditio Kamal Ibrahim");
+
+  print("Muhammad Aditio Kamal Ibrahim");
+
+  print("Muhammad Aditio Kamal Ibrahim");
+
+  print("Muhammad Aditio Kamal Ibrahim");
+
+}
