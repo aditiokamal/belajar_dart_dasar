d5d8697715fd5dd15eaa9958a76432419e4ea974
aditiokamal <aditiokamal@gmail.com>
2022-05-11 15:41:53 +0700

    belajar penggunaan kata kunci var, update file variable.dart

--- bin/variable.dart
+++ bin/variable.dart
@@ -16,7 +16,7 @@
 
   String name;
 
-  name='Muhammad Aditio Kamal Ibrahim';
+  name='2 Muhammad Aditio Kamal Ibrahim';
 
   print(name);
 
@@ -29,8 +29,17 @@
 }*/
 
 //menggunakan variable deklarasi langsung
+/*void main(){
+  String name='3 Muhammad Aditio Kamal Ibrahim';
+  print(name);
+  print(name);
+  print(name);
+  print(name);
+}*/
+
+//menggunakan kata kunci var untuk membuat variable langsung dengan nilainya
 void main(){
-  String name='Muhammad Aditio Kamal Ibrahim';
+  var name='4 Muhammad Aditio Kamal Ibrahim';
   print(name);
   print(name);
   print(name);
