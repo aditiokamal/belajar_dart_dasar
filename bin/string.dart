3e28024fb45990e5e8268f497c33ace89a4df186
aditiokamal <aditiokamal@gmail.com>
2022-05-25 13:24:12 +0700

    praktek penggunaan karakter backslash untuk perintah print; update file string.dart

--- bin/string.dart
+++ bin/string.dart
@@ -14,6 +14,7 @@ void main(){
 */
 
 //penggunaan string interpolation
+/*
 void main(){
 
   String firstName = 'Muhammad Aditio';
@@ -22,5 +23,19 @@ void main(){
   var fullName = '$firstName ${lastName}';
 
   print(fullName);
+
+}
+*/
+
+//penggunaan karakter backslash
+void main(){
+
+  String firstName = 'Muhammad Aditio';
+  String lastName = "Kamal Ibrahim";
+
+  var fullName = '$firstName \${lastName}';
+
+  print(fullName);
+  print('print tanda petik \'');
 
 }
