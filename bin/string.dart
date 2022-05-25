14f196ff332860aadec9deec2ef50e26fc1b07e5
aditiokamal <aditiokamal@gmail.com>
2022-05-25 13:36:51 +0700

    praktek cara menggabungkan string; update file string.dart

--- bin/string.dart
+++ bin/string.dart
@@ -28,6 +28,7 @@ void main(){
 */
 
 //penggunaan karakter backslash
+/*
 void main(){
 
   String firstName = 'Muhammad Aditio';
@@ -40,5 +41,23 @@ void main(){
 
   var text = 'this is \'dart\' \$cool';
   print(text);
+
+}
+*/
+
+//menggabungkaan string
+void main(){
+
+  String firstName = 'Aditio';
+  String lastName = "Kamal";
+
+  var name1 = firstName + lastName;
+  var name2 = 'Aditio' 'Kamal';
+  var name3 = firstName +' '+ lastName;
+
+
+  print(name1);
+  print(name2);
+  print(name3);
 
 }
