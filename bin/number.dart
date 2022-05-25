8b94ce5300bb34a2e02cd22d898945671acdfe8c
aditiokamal <aditiokamal@gmail.com>
2022-05-25 12:56:42 +0700

    praktek penggunaan TipeData int,double, dan num; add file number.dart

---
+++ bin/number.dart
@@ -0,0 +1,34 @@
+//penggunaan TipeData int dan double
+/*
+void main(){
+
+  int number1 = 10;
+  double number2 = 10.5;
+
+  print(number1);
+  print(number2);
+
+  // number1 = 20;
+  // print(number1);
+  // number2 = 20.5;
+  // print(number2);
+
+  // number1 = 30.5; //error karena TipeData int tidak dapat menampung desimal
+  // print(number1);
+
+  number2 = 40;
+  print(number2);
+
+}
+*/
+
+//penggunaan TipeData num
+void main(){
+
+  num number = 10;
+  print(number);
+
+  number = 10.5;
+  print(number);
+
+}
