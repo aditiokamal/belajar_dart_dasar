a56caf1d0ed8669c2d62fbcc37e5311ab180b8df
aditiokamal <aditiokamal@gmail.com>
2022-05-25 12:36:17 +0700

    praktek penggunnaan comment pada dart, add file comment.dart

---
+++ bin/comment.dart
@@ -0,0 +1,11 @@
+/// this is main function
+/// will executed by dart.exe
+void main(){
+  // this is name variable
+  var name = 'Muhammad Aditio Kamal Ibrahim';
+  /**
+   * this is multiple line comment
+   * you can add more here
+   */
+  print(name);
+}
