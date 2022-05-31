9b2c81e3d7caf5671564c79ea48f04a0c1e3c897
aditiokamal <aditiokamal@gmail.com>
2022-05-31 09:34:37 +0700

    praktek konversi TipeData; update conversion.dart

--- bin/conversion.dart
+++ bin/conversion.dart
@@ -2,17 +2,49 @@
 void main(){
 
   var inputString = '1000';
-  var inputInt = int.parse(inputString);
-  var inputDouble = double.parse(inputString);
+  var stringToInt = int.parse(inputString);
+  var stringToDouble = double.parse(inputString);
 
   print(inputString);
-  print(inputInt);
-  print(inputDouble);
+  print(stringToInt);
+  print(stringToDouble);
 
-  var intToDoble = inputInt.toDouble();
-  var doubleToInt = inputDouble.toInt();
+  var intToDoble = stringToInt.toDouble();
+  var doubleToInt = stringToDouble.toInt();
 
-  var intToString = inputInt.toString();
-  var doubleToString = inputDouble.toString();
+  print(intToDoble);
+  print(doubleToInt);
+
+  var intToString = stringToInt.toString();
+  var doubleToString = stringToDouble.toString();
+
+  print(intToString);
+  print(doubleToString);
+
+  var inputBool = true;
+  var boolToString = inputBool.toString();
+  var stringToBool = 'ga ada, pake operator perbandingan';
+
+  //test
+  print('Input : $inputString');
+  print('Konversi dari String ke Int : $stringToInt');
+  print('Konversi dari String ke Double : $stringToDouble');
+  print('Konversi dari String ke Boolean : $stringToBool');
+
+  print('Konversi dari Int ke String  : $intToString');
+  print('Konversi dari Double ke String : $doubleToString');
+  print('Konversi dari Boolean ke String : $boolToString');
+
+  print('Konversi dari Int ke Double : $intToDoble');
+  print('Konversi dari Double ke Int : $doubleToInt');
+
+  print('Konversi dari Int ke Boolean : \$intToBool');
+  print('Konversi dari Double ke Boolean : \$doubleToBool');
+
+  print('Konversi dari Boolean ke Int : \$boolToInt');
+  print('Konversi dari Boolean ke Double : \$boolToDouble');
+
+
+
 
 }
