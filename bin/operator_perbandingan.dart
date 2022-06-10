913ae0862c64f459bc689b2800eeb521aca40f11
aditiokamal <aditiokamal@gmail.com>
2022-06-10 07:21:29 +0700

    praktek penggunaan operator perbandingan; add file operator_perbandingan.dart

---
+++ bin/operator_perbandingan.dart
@@ -0,0 +1,12 @@
+//praktek penggunaan operator perbandingan
+void main() {
+  print(10 == 10); //operator perbandingan sama dengan
+  print(10 != 10); //operator perbandingan tidak sama dengan
+  print(10 < 10); //operator perbandingan kurang dari
+  print(10 > 10); //operator perbandingan lebih dari
+  print(10 <= 10); //operator perbandingan kurang dari sama dengan
+  print(10 >= 10); //operator perbandingan lebih darii sama dengan
+
+  print('test' == 'test'); //operator perbandingan sama dengan
+  print('test' != 'test'); //operator perbandingan tidak sama dengan
+}
