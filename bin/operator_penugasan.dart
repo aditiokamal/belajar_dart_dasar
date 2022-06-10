3689800110414fa01a0460251fbcd898575c6fa2
aditiokamal <aditiokamal@gmail.com>
2022-06-10 07:37:05 +0700

    praktek penggunaan operator perbandingan; add file operator_penugasan.dart

---
+++ bin/operator_penugasan.dart
@@ -0,0 +1,17 @@
+//praktek penggunaan operator perbandingan /Augmented Assignments
+void main() {
+  var a = 10;
+
+  //a = a + 10;  //tanpa operator penugasan
+  a += 10; //menggunakan operator penugasan
+
+  /*operator penugasan yang tersedia
+  a += 10;
+  a -= 10;
+  a *= 10;
+  a /= 10;
+  a ~/= 10;
+  a *= 10;
+*/
+  print(a);
+}
