0560fe306e00a1d64900be0517a503f67595be91
aditiokamal <aditiokamal@gmail.com>
2022-06-10 09:21:17 +0700

    praktek penggunaan list; add file list.dart

---
+++ bin/list.dart
@@ -0,0 +1,60 @@
+//praktek penggunaan list
+/*contoh penggunaan List
+
+  //Untuk membuat list kita bisa tentukan tipe datanya, misal:
+  List<TipeData> namaVariable = [];
+
+  //Atau bisa menggunakan kata kunci var atau final
+  var namaVariable = <TipeData>[];
+  final namaVariable = <TipeData>[];
+
+
+  //create List of int
+  List<int> ListInt = [];
+
+  //create List of String
+  var ListString  = <String>[];
+ */
+void main() {
+  List<int> ListInt = [];
+
+  var ListString = <String>[];
+
+  print(ListInt);
+  print(ListString);
+
+  var names = <String>[];
+
+  names.add('Muhammad');
+  names.add('Aditio');
+  names.add('Kamal');
+  names.add('Ibrahim');
+  names.add('Test');
+  names.add('HAPUS');
+
+  print(names);
+  print(names.join(' '));
+  print(names.length); //cek length,
+  print(names[1]); //print index ke-1
+
+  names[4] = 'TEST'; //ubah index ke-4
+  print(names[4]); //cetak index ke-4
+  names.removeAt(5); //hapus index ke-5
+  print(names); //cetak List
+
+  var names2 = [
+    'index-0',
+    'index-1',
+    'index-2',
+  ];
+
+  print(names2);
+
+  var names3 = <String>[
+    'index-0',
+    'index-1',
+    'index-2',
+  ];
+
+  print(names3);
+}
