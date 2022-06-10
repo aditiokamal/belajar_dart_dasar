2c8ce677bc559a086b8fd27b27bbc131c109b22e
aditiokamal <aditiokamal@gmail.com>
2022-06-10 07:09:48 +0700

    praktek penggunaan operator aritmatika; add file operator_aritmatika.dart

---
+++ bin/operator_aritmatika.dart
@@ -0,0 +1,11 @@
+void main() {
+  var first = 10;
+  var second = 3;
+
+  print(first + second); //operator aritmatika penambahan
+  print(first - second); //operator aritmatika pengurangan
+  print(first * second); //operator aritmatika perkalian
+  print(first / second); //operator aritmatika pembagian hasil Double
+  print(first ~/ second); //operator aritmatika pembagian hasil Integer
+  print(first % second); //operator aritmatika modulus/sisa bagi
+}
