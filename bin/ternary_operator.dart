9782054d8988c799ec0c75034ddcd78f5fb8e589
aditiokamal <aditiokamal@gmail.com>
2022-06-13 10:25:35 +0700

    praktek penggunaan ternary operator; add file ternary_operator.dart

---
+++ bin/ternary_operator.dart
@@ -0,0 +1,17 @@
+//praktek penggunaan ternary operator
+void main() {
+  var nilai = 90;
+  var ucapan = nilai >= 75 ? 'Selamat Anda lulus' : 'Anda Tidak Lulus'; //penggunaan ternary operator
+
+  /* jika menggunakan if
+  String ucapan;
+
+  if (nilai >= 75) {
+    ucapan = 'Selanat Anda Lulus';
+  } else{
+    ucapan = 'Anda Tidak Lulus';
+  }
+*/
+
+  print(ucapan);
+}
