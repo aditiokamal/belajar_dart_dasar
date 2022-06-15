695ed924e15b205cc5cd423fa1b3d5e96badb6c3
aditiokamal <aditiokamal@gmail.com>
2022-06-15 10:31:24 +0700

    praktek penggunaan while loop; add file while.dart

---
+++ bin/while.dart
@@ -0,0 +1,23 @@
+//praktek penggunaan while
+/* While Loop
+* while loop adalah versi perulangan yang lebih sederhana dibanding for loop
+* di while loop, hanya terdapat kondisi perulangan, tanpa ada init statement dan post statement
+*
+* syntax
+*   while (/*kondisi_perulangan*/){
+*     /*blok_perulangan*/
+*   }
+*
+* urutan proses:
+* selama kondisi_perulangan bernilai TRUE, maka run blok_perulangan;
+* jika kondisi_perulangan bernilai FALSE, maka end WHILE tanpa run blok_perulangan;
+* */
+
+void main() {
+  var counter = 1;
+
+  while (counter <= 10) {
+    print('Perulangan Ke-$counter');
+    counter++;
+  }
+}
