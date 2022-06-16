0a0f586c06554201ceffa4841b84b9858d0ef1ae
aditiokamal <aditiokamal@gmail.com>
2022-06-16 23:11:08 +0700

    praktek penggunaan function optional parameter default value;add file function_optional_default value.dart

---
+++ bin/function_optional_parameter_default_value.dart
@@ -0,0 +1,32 @@
+//praktek penggunaan function optional parameter default value
+/*
+
+Function Optional Parameter Default Value
+
+jika optional parameter tidak ingin nullable, maka kita wajib menambahkan default value
+caranya, setelah namaParameter, kita tambahkan = default_value
+
+syntax
+- membuat function optional parameter default value
+
+  void namaFunction(TypeData namaParameter1, [TypeData  optionalParameter1 = default_value, TypeData?  optionalParameter2 = default_value]) {
+    print('Hello $namaParameter1 $optionalParameter1 $optionalParameter2');
+  }
+
+- memanggil function optional parameter default value
+
+  void main()  {
+    namaFunction(namaParameter1, optionalParameter1, optionalParameter2);
+  }
+
+ */
+
+void sayHello(String firstName, [String middleName = '', String lastName = '']) {
+  print('Hello $firstName $middleName $lastName');
+}
+
+void main() {
+  sayHello('Muhammad');
+  sayHello('Muhammad', 'Aditio Kamal');
+  sayHello('Muhammad', 'Aditio Kamal', 'Ibrahim');
+}
