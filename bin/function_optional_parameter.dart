b5c87fd294da17b49d36c2d598884a700375b47d
aditiokamal <aditiokamal@gmail.com>
2022-06-16 23:01:27 +0700

    praktek penggunaan function optional parameter; add file function_optional_parameter

---
+++ bin/function_optional_parameter.dart
@@ -0,0 +1,34 @@
+//praktek penggunaan function optional parameter
+/*
+
+Function Optional Parameter
+
+secara default, parameter wajib dikirim ketika kita memanggil function
+namun jika kita ingin membuat parameter yang optional, artinya tidak wajib dikirim,
+kira bisa wrap dalam kurung []
+dan parameter optional HARUS NULLABLE
+optional parameter tidak bisa diletakan sebagai parameter pertama
+
+syntax
+- membuat function dengan optional parameter
+
+  void namaFunction(TypeData namaParameter1, [TypeData?  optionalParameter1, TypeData?  optionalParameter2]) {
+    print('Hello $namaParameter1 $optionalParameter1 $optionalParameter2');
+  }
+
+- memanggil function dengan optional parameter
+
+  void main()  {
+    namaFunction(namaParameter1, optionalParameter1, optionalParameter2);
+  }
+
+ */
+void sayHello(String firstName, [String? middleName,String? lastName]) {
+  print('Hello $firstName $middleName $lastName');
+}
+
+void main() {
+  sayHello('Muhammad');
+  sayHello('Muhammad', 'Aditio Kamal');
+  sayHello('Muhammad', 'Aditio Kamal', 'Ibrahim');
+}
